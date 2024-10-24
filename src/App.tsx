cbd9839ad9a07715f1e4f81c94ae618522cd7093
Mathieu_Prier <mathieu.priez@live.fr>
2024-10-24 10:27:13 +0200

    correction branche

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import "./App.css";
-import PokemonCard from "./components.1/PokemonCArd";
+import PokemonCard from "./components.1/PokemonCard";
+
+const pokemonList = [
+	{
+		name: "bulbasaur",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+		name: "mew",
+	},
+];
+
 function hello() {
-	return <PokemonCard />;
+	return <PokemonCard pokemon={pokemonList[1]} />;
 }
 
 export default hello;
