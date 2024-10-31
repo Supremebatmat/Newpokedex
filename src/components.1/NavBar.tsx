8e8b65249000b3b1ed3c25a6f4f307cd286da2c9
Mathieu Priez <mathieu.priez@live.fr>
2024-10-31 11:43:30 +0100

    Update NavBar.tsx

--- src/components.1/NavBar.tsx
+++ src/components.1/NavBar.tsx
@@ -1,16 +1,27 @@
-
-
-function Navbar(){
-
-
-
-
-return
-
-
-
+interface Pokemon {
+	name: string;
+	imgSrc?: string;
 }
 
+interface NavBarProps {
+	setPokemonIndex: (index: number) => void;
+	pokemonList: Pokemon[];
+}
 
+function NavBar({ setPokemonIndex, pokemonIndex, pokemonList }): NavBarProps {
+	return (
+		<nav>
+			{pokemonList.map((pokemon, index) => (
+				<button
+					key={pokemon.name}
+					type="button"
+					onClick={() => setPokemonIndex(index)}
+				>
+					{pokemon.name}
+				</button>
+			))}
+		</nav>
+	);
+}
 
-export default Navbar
+export default NavBar;
